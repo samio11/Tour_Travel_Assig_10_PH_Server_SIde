04b62894d79d3ed7563fc609eaf9f85ab1f8be87
Samio Hasan <138763106+samio11@users.noreply.github.com>
2024-06-29 17:00:30 +0600

    Search by email and country added

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Travel_Agency_A_10
-// 7IA8BueziMcM6Aim
 
 app.use(cors());
 app.use(express.json())
@@ -15,29 +13,63 @@ app.get('/', (req, res) => {
 })
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.mmutbdd.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+    try {
+        const database = client.db('Travel_Agency').collection('Travel_Info')
+        const countryData = client.db('Travel_Agency').collection('Country')
+
+        app.post('/travel_info', async (req, res) => {
+            const data = req.body;
+            const result = await database.insertOne(data);
+            res.send(result);
+        })
+        app.get('/', (req, res) => {
+            res.send(`${process.env.USER_NAME} hello world`)
+        })
+
+        app.get('/travel_info',async (req,res)=>{
+            const result = await database.find().toArray();
+            res.send(result);
+        })
+        app.get('/travel_info/:id',async (req,res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await database.findOne(query);
+            res.send(result);
+        })
+
+        app.get('/country_info/:country', async (req, res) => {
+            const result = await database.find({countryName:req.params.country}).toArray();
+            res.send(result);
+        })
+        app.get('/my_tour/:email', async (req, res) => {
+            const result = await database.find({userEmail:req.params.email}).toArray();
+            res.send(result);
+        })
+         
+       app.get('/country',async (req,res)=>{
+        const result = await countryData.find().toArray();
+        res.send(result);
+       })
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
